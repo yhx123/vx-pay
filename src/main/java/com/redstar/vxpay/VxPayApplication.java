ae20af816343e3dc9e20e7420df7c4d6590ebddd
yhx123 <714565396@qq.com>
2018-05-06 11:41:19 +0800

    Initial commit

---
+++ src/main/java/com/redstar/vxpay/VxPayApplication.java
@@ -0,0 +1,13 @@
+package com.redstar.vxpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class VxPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VxPayApplication.class, args);
+    }
+}
---
+++ src/test/java/com/redstar/vxpay/VxPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redstar.vxpay;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class VxPayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
